4ed161bfa05745e75d4fbcfdac6807d1cf2adaa9
evdokimov-dens <evdokimov.denis96@yandex.ru>
2025-08-30 00:35:44 +0500

    add: hero content

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 2,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000,
+  },
+});
