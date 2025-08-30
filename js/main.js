cb1edecd44ceefc02fd8ad4d2b9cb30e6403d724
evdokimov-dens <evdokimov.denis96@yandex.ru>
2025-08-31 02:19:42 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const swiper = new Swiper(".swiper", {
-  slidesPerView: 2,
+  slidesPerView: 3,
   loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 0,
   },
 });
