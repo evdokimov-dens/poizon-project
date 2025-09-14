f31d2cdbdf3030ef9a46284b22088277bd33debf
evdokimov-dens <evdokimov.denis96@yandex.ru>
2025-09-14 23:26:19 +0500

    add: php

---
+++ header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Kantumruy" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <title>Poizon</title>
+  </head>
+  <body>
+    <nav class="navbar">
+      <a href="/index.html" class="header-logo">
+        <svg class="logo-svg" width="49" height="35">
+          <use href="img/sprite.svg#logo"></use>
+        </svg>
+      </a>
+      <ul class="header-nav">
+        <li class="header-nav-item">
+          <a href="#" class="header-nav-link">Одежда</a>
+        </li>
+        <li class="header-nav-item">
+          <a href="#" class="header-nav-link">Обувь</a>
+        </li>
+        <li class="header-nav-item">
+          <a href="#" class="header-nav-link">Аксессуары</a>
+        </li>
+        <li class="header-nav-item">
+          <a href="#" class="header-nav-link">Калькулятор стоимости</a>
+        </li>
+        <li class="header-nav-item">
+          <a href="#" class="header-nav-link">Помощь</a>
+        </li>
+        <li class="header-nav-item">
+          <a href="#" class="header-nav-link">Подобрать размер</a>
+        </li>
+      </ul>
+      <div class="header-basket-batton">
+        <a href="#" class="header-basket">
+          <svg class="basket" width="22" height="22">
+            <use href="img/sprite.svg#basket"></use>
+          </svg>
+        </a>
+        <button class="header-button">
+          <a href="#" class="header-button-logo-svg">
+            <svg class="header-button-logo" width="29" height="21">
+              <use href="img/sprite.svg#logoButton"></use>
+            </svg>
+          </a>
+          <div class="header-button-text">
+            <span class="header-button-text1">
+              <a href="#" class="header-button-link1">Наш телеграм канал</a>
+            </span>
+            <span class="header-button-text2">
+              <a href="#" class="header-button-link2">Telegram web app</a>
+            </span>
+          </div>
+        </button>
+      </div>
+    </nav>
