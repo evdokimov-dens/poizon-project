20f3ef5aa9b4c75341852cd58d5427260b3c77b7
evdokimov-dens <evdokimov.denis96@yandex.ru>
2025-09-18 23:41:53 +0500

    fix: live server

--- exchange.php
+++ exchange.php
@@ -7,6 +7,15 @@
       <h2 class="exchange-block-title">Обмен и возврат</h2>
     </div>
     <h3 class="exchange-title-mini">Обмен</h3>
+    <h3 class="how-exchange-title">Как запросить обмен?</h3>
+    <p class="how-exchange-text">Для того, чтобы запросить обмен, напишите нам на адрес электронной почты refunds@thesortage.com письмо с
+темой "Обмен". В письме необходимо обязательно указать: </p>
+    <ul class="how-exchange-list">
+      <li class="how-exchange-item">номер вашего заказа, который можно найти в личном кабинете, или, если вы оформляли заказ в качестве гостя,
+      в электронном письме с подтверждением заказа;</li>
+      <li class="how-exchange-item">контактный номер телефона.</li>
+    </ul>
+    <p class="how-exchange-text">После получения письма с вами по телефону свяжется наш сотрудник.</p>
   </div>
 </section>
 
---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+cd C:\OSPanel\domains\poizon-project
+echo Запуск BrowserSync...
+browser-sync start --proxy "poizon-project" --files "**/*" --startPath "/exchange"
+pause
