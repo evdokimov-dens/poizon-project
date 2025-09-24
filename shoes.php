9b9baa4d145db54ec51c73d7c9375dddcc57ad68
evdokimov-dens <evdokimov.denis96@yandex.ru>
2025-09-24 23:43:55 +0500

    add: shoes page

--- shoes.php
+++ shoes.php
@@ -1,6 +1,8 @@
 <? include_once ('header.php');?>
 <section class="shoes-content">
-  <h2 class="shoes-title">Обувь</h2>
-  <p class="shoes-text">Главная / Обувь</p>
+  <div class="shoes-block">
+    <h2 class="shoes-title">Обувь</h2>
+    <p class="shoes-text">Главная / Обувь</p>
+  </div>
 </section>
 <? include_once('footer.php');?>
