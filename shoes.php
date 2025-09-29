4bc1d5dd410fd317a784f1ab164a8beb8ae06038
evdokimov-dens <evdokimov.denis96@yandex.ru>
2025-09-30 00:39:52 +0500

    add: shoes container

--- shoes.php
+++ shoes.php
@@ -4,5 +4,18 @@
     <h2 class="shoes-title">Обувь</h2>
     <p class="shoes-text">Главная / Обувь</p>
   </div>
+  <div class="price-filter-container">
+    <button class="shoes-costs">3 076₽ - 334 485 ₽</button>
+    <button class="shoes-brand">Бренд
+      <svg class="shoes-arrow-vector" width="19" height="19">
+            <use href="img/sprite.svg#shoes-arrow"></use>
+          </svg>
+    </button>
+    <button class="shoes-size">Размер
+      <svg class="shoes-arrow-vector" width="19" height="19">
+            <use href="img/sprite.svg#shoes-arrow"></use>
+          </svg>
+    </button>
+  </div>
 </section>
 <? include_once('footer.php');?>
--- start.bat
+++ start.bat
@@ -3,3 +3,5 @@ cd C:\OSPanel\domains\poizon-project
 echo Запуск BrowserSync...
 browser-sync start --proxy "poizon-project" --files "**/*" --startPath "/exchange"
 pause
+
+@REM browser-sync start --proxy "http://poizon-project" --files "css/*.css, js/*.js, *.php"
