9d26c585d7ade3c9fcc37f40f583a615caa6b727
evdokimov-dens <evdokimov.denis96@yandex.ru>
2025-09-30 23:09:18 +0500

    add: price filter

--- shoes.php
+++ shoes.php
@@ -4,18 +4,25 @@
     <h2 class="shoes-title">Обувь</h2>
     <p class="shoes-text">Главная / Обувь</p>
   </div>
-  <div class="price-filter-container">
+  <div class="price-container">
     <button class="shoes-costs">3 076₽ - 334 485 ₽</button>
     <button class="shoes-brand">Бренд
-      <svg class="shoes-arrow-vector" width="19" height="19">
-            <use href="img/sprite.svg#shoes-arrow"></use>
-          </svg>
-    </button>
+  <svg width="19" height="19" viewBox="0 0 20 20" fill="none">
+    <path d="M5 7L10 12L15 7" stroke="#000" stroke-width="2"/>
+  </svg>
+</button>
     <button class="shoes-size">Размер
-      <svg class="shoes-arrow-vector" width="19" height="19">
-            <use href="img/sprite.svg#shoes-arrow"></use>
-          </svg>
+      <svg width="19" height="19" viewBox="0 0 20 20" fill="none">
+    <path d="M5 7L10 12L15 7" stroke="#000" stroke-width="2"/>
+  </svg>
     </button>
+  </div>
+  <div class="price-filter-container">
+    <p class="price-filter-text">Цена</p>
+    <div class="range-wrapper">
+      <input class="custom-range" type="range" min="0" max="334485" step="1" value="0">
+      <input class="custom-range" type="range" min="0" max="334485" step="1" value="334485">
+    </div>
   </div>
 </section>
 <? include_once('footer.php');?>
