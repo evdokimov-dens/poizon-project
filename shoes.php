0757b0ffca21cc25b910e156e6f39c3a7d84b42d
evdokimov-dens <evdokimov.denis96@yandex.ru>
2025-10-03 00:16:54 +0500

    add: checkbox content

--- shoes.php
+++ shoes.php
@@ -48,5 +48,149 @@
       </button>
       </div>
   </div>
+  <div class="checkbox-content">
+    <div class="checkbox-content-block">
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        Adidas
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        Converse
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        New Balance
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        The North Face
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        Timberland
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        Crocs
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        Dr Martens
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        Native shoes
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        Nike
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        Puma
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        Under Armour
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        Asics
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        Li-ning
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        Onitsuka Tiger
+    </label>
+    </div>
+    <div class="footer-price-filter">
+      <button class="footer-price-filter-button-reset">
+        <p class="footer-price-filter-text1">Сбросить</p>
+      </button>
+      <button class="footer-price-filter-button-complite">
+        <p class="footer-price-filter-text">Готово</p>
+      </button>
+      </div>
+  </div>
+  <div class="checkbox-content-size">
+    <div class="checkbox-content-block">
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        36
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        37
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        38
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        39
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        40
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        41
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        42
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        43
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        44
+    </label>
+    <label class="checkbox-item">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+        45
+    </label>
+    </div>
+    <div class="footer-price-filter">
+      <button class="footer-price-filter-button-reset">
+        <p class="footer-price-filter-text1">Сбросить</p>
+      </button>
+      <button class="footer-price-filter-button-complite">
+        <p class="footer-price-filter-text">Готово</p>
+      </button>
+      </div>
+  </div>
 </section>
 <? include_once('footer.php');?>
